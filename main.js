9ec2c798a0c34a30a4953ea67969efdb7d5ca364
eadarasaisamhita <72115037+eadarasaisamhita@users.noreply.github.com>
2020-11-13 16:28:56 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX=0;
+noseY=0;
+ fancy_mustache = loadImage('handlebar-moustache-clip-art-mustache.jpg')
 function preload() {
 
 }
@@ -23,6 +26,8 @@ function gotPoses()
     if(results.length > 0)
     {
         console.log(results)
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
         console.log("nose x =" + results [0].pose.nose.x);
         console.log("nose y =" + results [0].pose.nose.y);
     }
@@ -30,6 +35,9 @@ function gotPoses()
 
 function draw() {
  image(video, 0, 0, 300, 300)
+ fill(255,0,0)
+ stroke(255,0,0)
+ image(fancy_mustache, noseX, noseY, 30,30)
 }
 
 function take_snapshot(){
